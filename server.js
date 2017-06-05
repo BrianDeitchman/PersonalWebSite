436236ec4a367158466bdd26e41824ec4b4bfaee
Brian Deitchman <BrianDeitchman>
2017-06-04 23:40:52 -0700

    heroku update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use('/',(req,res) => {
  res.send("Hello world")
 });
-
-app.listen(4200, ()=> {
+const port = process.env.PORT || 4200;
+app.listen(port, ()=> {
     console.log('listening on *:4200');
 });
